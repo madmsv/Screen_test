e3515da5fdbd8fae6d025e90cbee6187d5529fc5
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-04-20 15:25:02 +0200

    working code

--- components/Display/include/Display.h
+++ components/Display/include/Display.h
@@ -9,16 +9,16 @@
 #include "driver/i2c_master.h"
 
 #include "esp_lcd_io_i2c.h"
-#include "esp_lcd_panel_vendor.h"
-// #include "esp_lcd_panel_ssd1306.h"
-#include "esp_lcd_sh1107.h"
+// #include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_sh1106.h"
+// #include "esp_lcd_sh1107.h"
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
 
 #include "esp_lvgl_port.h"
 #include "lvgl.h"
 
-#define EXAMPLE_LVGL_PALETTE_SIZE      8
+// #define EXAMPLE_LVGL_PALETTE_SIZE      8
 
 /// @brief Most members must be set, except for `master_bus_handle`
 typedef struct Display_config{
--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@ void app_main(void)
     Display_config disp_cfg = {
         .dev_addr = 0x3C, //0x3C
         .height_res = 64,
-        .width_res = 132,
+        .width_res = 128,
         .io_scl = GPIO_NUM_12,
         .io_sda = GPIO_NUM_11,
         .port_num = 1,
